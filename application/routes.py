6df915253f4e64fe6b7bf176318a25e1bffc104b
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-29 09:25:33 +0100

    Add to and retrieve (all) from ElasticSearch

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,42 @@
 from application import app
-from flask import Response
+from flask import Response, request
+import requests
+from elasticsearch import Elasticsearch
+import logging
+import json
+
+elastic = Elasticsearch()
+
 
 @app.route('/', methods=["GET"])
 def index():
-	return Response(status=200)
+    return Response(status=200)
+
+
+@app.route('/entry', methods=['POST'])
+def add_index_entry():
+    if request.headers['Content-Type'] != "application/json":
+        return Response(status=415)
+
+    data = request.get_json(force=True)
+    logging.info(data)
+
+    for item in data:
+        result = elastic.index(index='index', doc_type='names', body=item)
+        logging.info(result['created'])
+    elastic.indices.refresh(index="index")
+    return Response(status=201)
+
+
+@app.route('/index', methods=['GET'])
+def get_all():
+    result = elastic.search(index='index', body={'query': {'match_all': {}}})
+
+    logging.info("Got %d hits".format(result['hits']['total']))
+    logging.info(result)
+
+    return_data = []
+    for hit in result['hits']['hits']:
+        return_data.append(hit['_source'])
+
+    return Response(json.dumps(return_data), status=200, mimetype='application/json')
